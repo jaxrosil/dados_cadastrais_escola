e256fd420fc1be5aa48ed357d1f639dd48b2e19f
Jax Rocha <jaxrosil98@gmail.com>
2021-12-29 22:15:42 -0300

    Restruturação do código
    Código refeito desde o início para mais segurança com o gitignore e com ambiente virtualizado.

--- setup/urls.py
+++ setup/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import AlunosViewSet, CursosViewSet
+from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunosViewSet, basename='Alunos')
 router.register('cursos', CursosViewSet, basename='Cursos')
+router.register('matriculas', MatriculasViewSet, basename='Matrículas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(router.urls)),
+    path('', include(router.urls)),
 ]
